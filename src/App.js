feb48f6379438d1854318baa8f5a6eb22c0fb2fe
qobulovasror <qobulovasror0@gmail.com>
2023-05-25 13:37:30 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello word</h1>
       </div>
     );
   }
 }
 
 export default App;
+//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
